54ac2be98fbbceacdedd38df7ed9c99bbad1860b
Shireen Garcia <garciasal@gmail.com>
2017-12-01 16:01:52 -0800

    Added main image, changed Songs to Other Chants

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 // Components
+import aochants from '../images/aochants.jpg';
+
+// Material-UI Components
+import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+const cardStyle= {
+  maxWidth: '50%',
+  maxHeight: '70%'
+}
 
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>This is the Home component</h1>
+      <div className="wrapper">
+        <div className="container">
+          <Card style={cardStyle}>
+            <CardMedia>
+              <img src={aochants} alt="The American Outlaws in Sandy, Utah" />
+            </CardMedia>
+            <CardTitle title="Welcome to AO Chants!" />
+            <CardText>
+              I created AO Chants as a way to have a quick reference to all of the American Outlaws chants and songs. Our job, as American Outlaws, is to positively support our team!
+            </CardText>
+          </Card>
+        </div>
       </div>
     );
   }
