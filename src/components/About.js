b03f75d25cbb46f94c728de5a133a11dd90c5f57
Shireen Garcia <garciasal@gmail.com>
2017-12-05 03:03:54 -0800

    About component started, figured out how to change color on icons

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 // Components
+import aoMe from '../images/ao_me.jpg';
+
+// Material-UI Components
+import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+const cardStyle = {
+  maxWidth: '70%',
+  maxHeight: '100%',
+}
 
 class About extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>This is the About Component</h1>
+      <div className="wrapper">
+        <div className="container">
+          <Card style={cardStyle}>
+            <CardMedia>
+              <img src={aoMe} alt="Me, Attending a US Soccer friendly in Utah" />
+            </CardMedia>
+            <CardTitle style={{textAlign: "center"}} title="About AO Chants" />
+            <CardText>
+              My name is Shireen and I'm a member of the American Outlaws, an unofficial supporters group of the US National Soccer Team.  While I was at a US Men's friendly match in Utah, I was trying to find out the words to one of the chants.  The chants section on the AO website is messy and unwiedly.  I created AO Chants as an easier way to access the chants, and an exercise for me, as this is one of the first projects I've created since graduating from The Iron Yard.
+            </CardText>
+          </Card>
+        </div>
       </div>
     );
   }
--- src/components/PopularChants.js
+++ src/components/PopularChants.js
@@ -8,13 +8,13 @@ import MobileTearSheet from '../components/MobileTearSheet.js';
 
 // Material-UI Components
 import {List, ListItem} from 'material-ui/List';
-import ContentInbox from 'material-ui/svg-icons/content/inbox';
+// import ContentInbox from 'material-ui/svg-icons/content/inbox';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
-import ContentSend from 'material-ui/svg-icons/content/send';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+// import ContentSend from 'material-ui/svg-icons/content/send';
+// import ContentDrafts from 'material-ui/svg-icons/content/drafts';
 // import Divider from 'material-ui/Divider';
 // import ActionInfo from 'material-ui/svg-icons/action/info';
-
+import {red900, indigo900} from 'material-ui/styles/colors';
 
 class PopularChants extends Component {
   render() {
@@ -23,11 +23,11 @@ class PopularChants extends Component {
         <div className="container">
           <MobileTearSheet>
             <List>
-              <ListItem primaryText="We Love Ya" leftIcon={<ContentInbox />} />
-              <ListItem primaryText="I Believe That We Will Win" leftIcon={<ActionGrade />} />
-              <ListItem primaryText="US 'Til I Die'" leftIcon={<ContentSend />} />
-              <ListItem primaryText="You're Not Singing Anymore (Or Overthere)" leftIcon={<ContentDrafts />} />
-              <ListItem primaryText="Everywhere We Go" leftIcon={<ContentInbox />} />
+              <ListItem primaryText="We Love Ya" leftIcon={<ActionGrade color={red900} />} />
+              <ListItem primaryText="I Believe That We Will Win" leftIcon={<ActionGrade color={indigo900} />} />
+              <ListItem primaryText="US 'Til I Die" leftIcon={<ActionGrade color={red900} />} />
+              <ListItem primaryText="You're Not Singing Anymore/Over There" leftIcon={<ActionGrade color={indigo900} />} />
+              <ListItem primaryText="Everywhere We Go" leftIcon={<ActionGrade color={red900} />} />
             </List>
           </MobileTearSheet>
         </div>
